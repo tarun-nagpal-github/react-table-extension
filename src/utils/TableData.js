3316dd81d3ab36e517e0ccdca905aca368ba91f7
Tarun Nagpal <tarun.nagpal78@gmail.com>
2019-11-08 10:26:44 +0530

    Added Table Column - Entry

--- src/utils/TableData.js
+++ src/utils/TableData.js
@@ -1,27 +1,34 @@
 const data = [{
-    _id: 1,
-    name: 'Roy Agasthyan',
-    age: 26
-  },{
-    _id: 2,
-    name: 'Sam Thomason',
-    age: 22
-  },{
-    _id: 3,
-    name: 'Michael Jackson',
-    age: 36
-  },{
-    _id: 4,
-    name: 'Samuel Roy',
-    age: 56
-  },{
-    _id: 5,
-    name: 'Rima Soy',
-    age: 28
-  },{
-    _id: 6,
-    name: 'Suzi Eliamma',
-    age: 28
-  }];
+  _id: 1,
+  name: 'Roy Agasthyan',
+  age: 26,
+  aadharId: 786786
+},
+{
+  _id: 2,
+  name: 'Sam Thomason',
+  age: 22,
+  aadharId: 786786
+}, {
+  _id: 3,
+  name: 'Michael Jackson',
+  age: 36,
+  aadharId: 786786
+}, {
+  _id: 4,
+  name: 'Samuel Roy',
+  age: 56,
+  aadharId: 786786
+}, {
+  _id: 5,
+  name: 'Rima Soy',
+  age: 28,
+  aadharId: 786786
+}, {
+  _id: 6,
+  name: 'Suzi Eliamma',
+  age: 28,
+  aadharId: 786786
+}];
 
-  export { data };
+export { data };
